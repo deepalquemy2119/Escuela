2985621e8e354d54ec06255680409841e48f7c39
deepalquemy2119 <deepalquemy2119@gmail.com>
2024-04-09 22:32:55 -0300

    armado y generacion de DDBB_Escuela.sql

---
+++ Querys/escuela_DDBB.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 10-04-2024 a las 03:23:54
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `Escuela`
+--
+DROP DATABASE IF EXISTS `Escuela`;
+CREATE DATABASE IF NOT EXISTS `Escuela` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `Escuela`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Administracion`
+--
+
+CREATE TABLE `Administracion` (
+  `id` bigint(20) NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `task` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `notice` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `state` bit(1) NOT NULL DEFAULT b'0',
+  `password` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELACIONES PARA LA TABLA `Administracion`:
+--   `id`
+--       `Personas` -> `id`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Mensajes`
+--
+
+CREATE TABLE `Mensajes` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `description` varchar(250) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `creation` date NOT NULL,
+  `state` bit(1) NOT NULL DEFAULT b'0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELACIONES PARA LA TABLA `Mensajes`:
+--   `id`
+--       `Personas` -> `id`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Personas`
+--
+
+CREATE TABLE `Personas` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `last_name` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `fecha_nac` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `correo` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `state` bit(1) NOT NULL DEFAULT b'0',
+  `password` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELACIONES PARA LA TABLA `Personas`:
+--   `correo`
+--       `Tareas` -> `correo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Tareas`
+--
+
+CREATE TABLE `Tareas` (
+  `name` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `description` varchar(250) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `creation` date DEFAULT NULL,
+  `state` bit(1) NOT NULL DEFAULT b'0',
+  `pass_task` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `correo` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- RELACIONES PARA LA TABLA `Tareas`:
+--   `correo`
+--       `Personas` -> `correo`
+--
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Administracion`
+--
+ALTER TABLE `Administracion`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indices de la tabla `Mensajes`
+--
+ALTER TABLE `Mensajes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indices de la tabla `Personas`
+--
+ALTER TABLE `Personas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `email` (`correo`);
+
+--
+-- Indices de la tabla `Tareas`
+--
+ALTER TABLE `Tareas`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Mensajes`
+--
+ALTER TABLE `Mensajes`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `Personas`
+--
+ALTER TABLE `Personas`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `Administracion`
+--
+ALTER TABLE `Administracion`
+  ADD CONSTRAINT `Administracion_ibfk_1` FOREIGN KEY (`id`) REFERENCES `Personas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `Mensajes`
+--
+ALTER TABLE `Mensajes`
+  ADD CONSTRAINT `Mensajes_ibfk_1` FOREIGN KEY (`id`) REFERENCES `Personas` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `Personas`
+--
+ALTER TABLE `Personas`
+  ADD CONSTRAINT `Personas_ibfk_1` FOREIGN KEY (`correo`) REFERENCES `Tareas` (`correo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `Tareas`
+--
+ALTER TABLE `Tareas`
+  ADD CONSTRAINT `Tareas_ibfk_1` FOREIGN KEY (`correo`) REFERENCES `Personas` (`correo`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
