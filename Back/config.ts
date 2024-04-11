e3ada0fcd465eb0c99408022d04d799ae504ac68
deepalquemy2119 <deepalquemy2119@gmail.com>
2024-04-11 17:05:28 -0300

    creacion del sql, tablas y DDBB

--- Back/config.ts
+++ Back/config.ts
@@ -1,9 +1,9 @@
-//export const NODE_ENV = process.env.NODE_ENV || "development"
+
 
 import dotenv from 'dotenv'
 
 dotenv.config()
-// recordar cargar las variables del .env para su base de datos
+
 export const db = process.env.DB
 export const user = process.env.USER
 export const pass = process.env.PASSWORD
